72fd74c96d52fbb27bc447eb05595a4bd4b5914e
chazandrune <chazandrune@163.com>
2019-03-05 16:54:56 +0800

    测试

--- js/custom_ninjashare.js
+++ js/custom_ninjashare.js
@@ -280,6 +280,9 @@ function tongbu(e,obj){
 	}else{
 		e.html(val)
 	}
+	objchangefont("body","5578cf01d15f468c8d75307533698d07",function(){
+		alert("字体更新完毕")
+	})
 }
 
 //定义我的第几个忍者图片数字输出
@@ -429,11 +432,11 @@ function printimg(){
 	},10000)
 
 	//有字库更新文字字体
-	$.get("https://service.youziku.com/home/get", function (data) {
-		let partName = "ajax2";
-		console.log(data);
-		$youziku.submit(partName);
-	})
+	// $.get("https://service.youziku.com/home/get", function (data) {
+	// 	let partName = "ajax2";
+	// 	console.log(data);
+	// 	$youziku.submit(partName);
+	// })
 
 	var canvas = document.createElement("canvas");
 	canvas.width = 1136;
